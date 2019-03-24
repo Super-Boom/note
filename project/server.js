f2dd73b4255ce6d411e18cd2da8245e8877b4d6b
xzg <xzg248369@163.com>
2019-03-24 21:22:07 +0800

    移动端滚动穿透问题

--- project/server.js
+++ project/server.js
@@ -1,9 +1,39 @@
 var express = require('express');
+var os = require('os');
 var app = express();
-var host = '192.168.73.1'
-app.get('/',function(req,res){
-    res.send('hello world')
+var path = require('path');
+// var host = '192.168.73.1'
+
+function getIp() {
+    var ipStr,
+        infaces = os.networkInterfaces(),
+        bool = false;
+    for (var i in infaces) {
+        infaces[i].some(function (x) {
+            if ((x.family == 'IPv4') && (x.internal == false)) {
+                ipStr = x.address;
+                bool = true;
+                return true
+            }
+        })
+        if (bool) { break }
+    }
+    return ipStr
+}
+var ip = getIp();
+var host = ip;
+
+app.listen(3000, host, function () {
+    console.log(`web i running on ${host}:3000`);
 })
-app.listen(3000,host,function(){
-    console.log('port 3000');
+
+app.set('views',path.resolve(__dirname,'pages'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.engine( '.html', require( 'ejs' ).__express );
+app.get('/', function (req, res) {
+    res.render('index')
+})
+app.get('/scroll',(req,res)=>{
+    res.render('mobilescroll');
 })
